c059d85f5b1ddb7250274e1964ac9705b6290856
Sander Standaert <standaertsander@gmail.com>
2021-04-24 18:21:57 +0200

    Creating project with angular and javaspringboot

---
+++ src/main/java/edu/ap/group10/leapwebapp/LeapWebappApplication.java
@@ -0,0 +1,13 @@
+package edu.ap.group10.leapwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ap/group10/leapwebapp/LeapWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ap.group10.leapwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
