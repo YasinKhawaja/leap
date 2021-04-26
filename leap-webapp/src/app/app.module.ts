a2a1e761e9a7dd581de0d89ad7e85d9354c19d52
janelguera <72437590+janelguera@users.noreply.github.com>
2021-04-26 15:29:44 +0200

    lay-out home & login

--- leap-webapp/src/app/app.module.ts
+++ leap-webapp/src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: 'login', component: LoginComponent }
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes) 
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
