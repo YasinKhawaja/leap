a7facea5361a7051206754064a6b4065a1c98321
Verhaegen Stijn (s115595) <stijn.verhaegen@student.ap.be>
2021-04-27 11:15:26 +0200

    Revert "lay-out home & login"
    This reverts commit a2a1e761e9a7dd581de0d89ad7e85d9354c19d52

--- leap-webapp/src/app/app.module.ts
+++ leap-webapp/src/app/app.module.ts
@@ -1,31 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'home', component: HomepageComponent },
-  { path: 'login', component: LoginComponent }
-]
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes) 
+    AppRoutingModule
   ],
-  
   providers: [],
   bootstrap: [AppComponent]
 })
