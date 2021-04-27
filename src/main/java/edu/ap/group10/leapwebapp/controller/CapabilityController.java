9dd3aa12ff576c0d95f45394d38d51b06d0a3139
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2021-04-27 15:46:01 +0200

    lijst van alle capabilities toegevoegd

--- src/main/java/edu/ap/group10/leapwebapp/controller/CapabilityController.java
+++ src/main/java/edu/ap/group10/leapwebapp/controller/CapabilityController.java
@@ -2,8 +2,10 @@ package edu.ap.group10.leapwebapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.ap.group10.leapwebapp.jpa.Capability;
 import edu.ap.group10.leapwebapp.jpa.CapabilityRepository;
@@ -14,8 +16,14 @@ public class CapabilityController {
     private CapabilityRepository repository;
 
     @PostMapping("/capabilities/add")
-    public String saveCapability(@RequestParam("name") String name){
+    public @ResponseBody String saveCapability(@RequestParam("name") String name){
         repository.save(new Capability(name));
         return "saved!!";
     }
+
+    @GetMapping("/capabilities")
+    public @ResponseBody Iterable<Capability> getAllCapabilities(){
+        return repository.findAll();
+    }
+
 }
