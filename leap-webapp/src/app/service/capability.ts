68b5f288d05ab5b95e67c045ef7495a6807968fb
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2021-04-29 14:25:51 +0200

    overview off all capabilities with properties

--- leap-webapp/src/app/service/capability.ts
+++ leap-webapp/src/app/service/capability.ts
@@ -1,10 +1,35 @@
 import { HttpParams } from "@angular/common/http";
 
 export class Capability {
-    constructor(public name: string){}
+    public parentId: string
+    public level: string
+    public paceOfChange: string
+    public tom: string
+    public resourcesQuality: string
+    public informationQuality: string
+    public applicationFit: string
+    
+
+    constructor(public name: string){
+        this.parentId = '';
+        this.level = '';
+        this.paceOfChange = '';
+        this.tom = '';
+        this.resourcesQuality = '';
+        this.informationQuality = '';
+        this.applicationFit = '';
+
+    }
 
     getParams() : HttpParams{
         return new HttpParams()
-            .set('name', this.name);
+            .set('name', this.name)
+            .set('resourcesQuality', this.resourcesQuality)
+            .set('parentId', this.parentId)
+            .set('level', this.level)
+            .set('paceOfChange', this.paceOfChange)
+            .set('tom', this.tom)
+            .set('informationQuality', this.informationQuality)
+            .set('applicationFit', this.applicationFit)
     }
 }
