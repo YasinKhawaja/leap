f5a58e467e652513455bb93edfdfd45dd595212e
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2021-04-29 11:27:22 +0200

    Added an overview of all existing capabilities in the database

--- leap-webapp/src/app/capability/capability.component.ts
+++ leap-webapp/src/app/capability/capability.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Capability } from '../service/capability';
+import { CapabilityService } from '../service/capability.service';
 
 @Component({
   selector: 'app-capability',
   templateUrl: './capability.component.html',
   styleUrls: ['./capability.component.css']
 })
+
 export class CapabilityComponent implements OnInit {
 
-  constructor() { }
+  capabilities: Capability[]
+
+  constructor(private cs: CapabilityService) { 
+    this.capabilities = [];
+  }
 
   ngOnInit(): void {
-  }
+    this.cs.getAllCapabilities()
+               .subscribe(data => { this.capabilities = data }, 
+                          error => { console.error(error) })
+}
 
 }
