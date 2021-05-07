4104cf7d0bb93ca8cc84e4cf7ad7b2c27d86ae2e
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2021-05-07 13:59:23 +0200

    export capability map to pdf

--- frontend/src/app/components/export/export.component.ts
+++ frontend/src/app/components/export/export.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Capability } from '../../classes/capability/capability';
 import { CapabilityService } from '../../services/capability/capability.service';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-export',
@@ -19,5 +21,20 @@ export class ExportComponent implements OnInit {
     this.cs.getAllCapabilities()
                .subscribe(data => { this.capabilities = data }, 
                           error => { console.error(error) })
-}
+  }
+
+  onSubmit() {
+    let data = document.getElementById('pdf');  
+        html2canvas(data).then(canvas => {
+        const contentDataURL = canvas.toDataURL('image/png')  
+        let pdf = new jsPDF('p','mm','a4'); //Generates PDF in landscape mode
+        // let pdf = new jspdf('p', 'cm', 'a4'); Generates PDF in portrait mode
+        var imgWidth = 208;
+        // imgHeight nog beter aanpassen
+        // de canvas wordt niet helemaal meegenomen als er capabilities bijkomen waardoor de map groter wordt
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        pdf.addImage(contentDataURL, 'PNG', 0, 0, imgWidth, imgHeight);  
+        pdf.save('CapabilityMap.pdf');   
+      }); 
+  }
 }
