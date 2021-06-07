a3223a1becc5026f2e7873995fd366393601d6d6
Sander Standaert <standaertsander@gmail.com>
2021-06-07 11:53:31 +0200

    fixed company controller -> only application admins can accept/deny requests

--- backend/src/main/java/edu/ap/group10/leapwebapp/company/CompanyController.java
+++ backend/src/main/java/edu/ap/group10/leapwebapp/company/CompanyController.java
@@ -27,11 +27,11 @@ public class CompanyController {
   private ConfirmationTokenService confirmationTokenService;
 
   @Autowired
-	private MailService mailService;
+  private MailService mailService;
 
   @GetMapping("/companies")
-  public List<Company> getAllCompanies(@RequestParam String role){
-    if (role.equals("application admin")){
+  public List<Company> getAllCompanies(@RequestParam String role) {
+    if (role.equals("application admin")) {
       return companyService.getCompanies();
     } else {
       throw new AccessDeniedException("Only application admins can access this");
@@ -39,56 +39,64 @@ public class CompanyController {
   }
 
   @PostMapping("/companies")
-  public void addNewCompany(@RequestParam("vatNumber") String vatNumber
-      , @RequestParam("companyName") String companyName, @RequestParam("taxOffice") String taxOffice, @RequestParam("businessActivity") String businessActivity, @RequestParam("email") String email, @RequestParam("streetName") String streetName
-      , @RequestParam("houseNumber") Integer houseNumber, @RequestParam("postcode") Integer postcode, @RequestParam("city") String city, @RequestParam("country") String country){
+  public void addNewCompany(@RequestParam("vatNumber") String vatNumber,
+      @RequestParam("companyName") String companyName, @RequestParam("taxOffice") String taxOffice,
+      @RequestParam("businessActivity") String businessActivity, @RequestParam("email") String email,
+      @RequestParam("streetName") String streetName, @RequestParam("houseNumber") Integer houseNumber,
+      @RequestParam("postcode") Integer postcode, @RequestParam("city") String city,
+      @RequestParam("country") String country) {
 
-          Company n = new Company(vatNumber, companyName, email, streetName, houseNumber, postcode, city, country, businessActivity, taxOffice);
-        
-          companyService.addCompany(n);
-  
-          String token = confirmationTokenService.addConfirmationToken(n);
-  
-          String confirmationTokenString =  "http://localhost:4200/company/register/?id=" + n.getId() + "&token=" + token;
-  
-          Mail mail = new Mail();
-          mail.setSender("leapwebapp@gmail.com");
-          mail.setReceiver("standaertsander@gmail.com, stijnverhaegen@gmail.com, yasin.khawaja@student.ap.be");
-          mail.setSubject("New application from: " + companyName);
-          mail.setContent("Click on this link to view the request from: " + companyName + ".\n" + confirmationTokenString);
-          mailService.sendMail(mail);
+    Company n = new Company(vatNumber, companyName, email, streetName, houseNumber, postcode, city, country,
+        businessActivity, taxOffice);
+
+    companyService.addCompany(n);
+
+    String token = confirmationTokenService.addConfirmationToken(n);
+
+    String confirmationTokenString = "http://localhost:4200/company/register/?id=" + n.getId() + "&token=" + token;
+
+    Mail mail = new Mail();
+    mail.setSender("leapwebapp@gmail.com");
+    mail.setReceiver("standaertsander@gmail.com, stijnverhaegen@gmail.com, yasin.khawaja@student.ap.be");
+    mail.setSubject("New application from: " + companyName);
+    mail.setContent("Click on this link to view the request from: " + companyName + ".\n" + confirmationTokenString);
+    mailService.sendMail(mail);
   }
 
   @GetMapping("/companies/{token}")
-  public Company viewCompanyApplication(@PathVariable("token") String confirmationToken){
+  public Company viewCompanyApplication(@PathVariable("token") String confirmationToken, @RequestParam String role) {
+    if (role.equals("application admin")) {
+      ConfirmationToken token = confirmationTokenService.getConfirmationToken(confirmationToken);
+      if (token != null) {
+        Company c = companyService.findCompany(token.getCompany().getId());
 
-    ConfirmationToken token = confirmationTokenService.getConfirmationToken(confirmationToken);
-    if(token != null) {
-      Company c = companyService.findCompany(token.getCompany().getId());
-
-      if(c != null){
-        return c;
+        if (c != null) {
+          return c;
+        } else {
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found");
+        }
       }
-      else{
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found");
-      }
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+          "You do not have the permission to accept or refuse a company.");
+    } else {
+        throw new AccessDeniedException("Only application admins can access this");
     }
-    throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "You do not have the permission to accept or refuse a company.");
   }
 
   @PostMapping("/companies/{token}/applicationStatus")
-  public void companyApplicationResult(@PathVariable String token, @RequestParam("accepted") Boolean accepted) {
+  public void companyApplicationResult(@PathVariable String token, @RequestParam("accepted") Boolean accepted, @RequestParam String role) {
 
+    if (role.equals("application admin")) {
     ConfirmationToken verifyToken = confirmationTokenService.getConfirmationToken(token);
 
     Long companyID = verifyToken.getCompany().getId();
     Company company = companyService.findCompany(companyID);
 
     confirmationTokenService.deleteConfirmationToken(token);
-    
+
     String confirmationTokenString = "";
 
-    if (accepted.booleanValue()){
+    if (accepted.booleanValue()) {
       String tokenAdmin = confirmationTokenService.addConfirmationToken(company);
       confirmationTokenString = "http://localhost:4200/register-useradmin?token=" + tokenAdmin;
     } else {
@@ -99,10 +107,15 @@ public class CompanyController {
     mail.setSender("leapwebapp@gmail.com");
     mail.setReceiver(company.getEmail());
     mail.setSubject("Your registration has been reviewed.");
-    mail.setContent("The admin has decided to" + (accepted.booleanValue() ? " accept " : " deny ") + "your application.\n" +
-    (accepted.booleanValue() ? "You can register your user-admin by clicking on the following link: " + confirmationTokenString
-    : "If you think this was a mistake, then send an e-mail to leapwebapp@gmail.com"));
+    mail.setContent(
+        "The admin has decided to" + (accepted.booleanValue() ? " accept " : " deny ") + "your application.\n"
+            + (accepted.booleanValue()
+                ? "You can register your user-admin by clicking on the following link: " + confirmationTokenString
+                : "If you think this was a mistake, then send an e-mail to leapwebapp@gmail.com"));
 
     mailService.sendMail(mail);
+  } else {
+    throw new AccessDeniedException("Only application admins can access this");
+  }
   }
 }
--- frontend/src/app/components/company-request/company-request.component.ts
+++ frontend/src/app/components/company-request/company-request.component.ts
@@ -3,6 +3,7 @@ import { Company } from 'src/app/classes/company/company';
 import { CompanyService } from 'src/app/services/company/company.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { JwtService } from 'src/app/services/jwt/jwt.service';
 
 @Component({
   selector: 'app-company-request',
@@ -12,20 +13,22 @@ import Swal from 'sweetalert2';
 export class CompanyRequestComponent implements OnInit {
 
   company!: Company;
+  role: string;
 
-  constructor(private c: CompanyService,  private router: Router) { 
+  constructor(private c: CompanyService,  private router: Router, private jwt: JwtService) { 
    }
 
    accept(): void {
-    this.c.accept(true);
+    this.c.accept(true, this.role);
    }
 
    decline(): void{
-    this.c.accept(false);
+    this.c.accept(false, this.role);
    }
 
   ngOnInit(): void {
-    this.c.getCompany().subscribe(data => { 
+    this.role = this.jwt.checkRole();
+    this.c.getCompany(this.role).subscribe(data => { 
       this.company = data;
     },
     error => {
