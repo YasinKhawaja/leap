b424ad98ff5f216b3c7f9b0e64566a5fec4375e6
stijn.verhaegen@student.ap.be <stijn.verhaegen@student.ap.be>
2021-06-11 15:35:20 +0200

    some business controller tests

---
+++ backend/src/test/java/edu/ap/group10/leapwebapp/businessprocess/BusinessProcessControllerIntegrationTests.java
@@ -0,0 +1,102 @@
+package edu.ap.group10.leapwebapp.businessprocess;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import edu.ap.group10.leapwebapp.businessprocesses.BusinessProcess;
+import edu.ap.group10.leapwebapp.businessprocesses.BusinessProcessService;
+import edu.ap.group10.leapwebapp.capability.CapabilityRepository;
+import edu.ap.group10.leapwebapp.capability.CapabilityService;
+import edu.ap.group10.leapwebapp.company.Company;
+import edu.ap.group10.leapwebapp.company.CompanyRepository;
+import edu.ap.group10.leapwebapp.company.CompanyService;
+import edu.ap.group10.leapwebapp.environment.Environment;
+import edu.ap.group10.leapwebapp.environment.EnvironmentRepository;
+import edu.ap.group10.leapwebapp.environment.EnvironmentService;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@AutoConfigureMockMvc
+@TestInstance(Lifecycle.PER_CLASS)
+public class BusinessProcessControllerIntegrationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private EnvironmentRepository environmentRepository;
+
+    @Autowired
+    private BusinessProcessService businessProcessService;
+
+    private static final Company company = new Company("1", "Test Company", "sv@gmail.com", "kerkstraat", 3, 5,
+            "Mortsel", "België", "", "");
+
+    private static final Environment environment = new Environment("Test environment", company);
+    
+    private static final BusinessProcess businessProcess1 = new BusinessProcess("Sales", "Income of the year 2021", environment);
+    private static final BusinessProcess businessProcess2 = new BusinessProcess("HR", "Staff income", environment);
+
+
+    @BeforeAll
+    void setup() {
+        companyRepository.save(company);
+        environmentRepository.save(environment);
+
+        businessProcessService.addBusinessProcess(businessProcess1);
+        businessProcessService.addBusinessProcess(businessProcess2);
+    }
+
+    @Test
+    @WithMockUser
+    public void givenEnvironmentId_whenGetAllBussinessProcesses_returnsBusinessProcesses() throws Exception {
+
+        //given
+        Long environmentId = environment.getId();
+
+        //when
+        mvc.perform(get("/businessprocesses/{environmentid}", environmentId))
+        
+        //then
+        .andExpect(status().isOk()).andDo(print())
+        .andExpect(content().json("[{'name': 'Sales'}, {'name':'HR'}]"));
+    }
+
+    @Test
+    @WithMockUser
+    public void givenBusinessProcessId_whenGetBusinessProcess_returnsBusinessProcess() throws Exception {
+
+        //given
+        Long businessProcess1Id = businessProcess1.getId();
+
+        //when
+        mvc.perform(get("/businessprocess/{businessprocessid}", businessProcess1Id))
+        
+        //then
+        .andExpect(status().isOk()).andDo(print())
+        .andExpect(content().json("{'name': 'Sales'}"));
+    }
+    
+}
