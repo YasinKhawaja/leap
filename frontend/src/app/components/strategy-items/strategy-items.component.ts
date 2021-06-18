d6db1fcb2d1d778430b72df67785ba8671659da0
janelguera <72437590+janelguera@users.noreply.github.com>
2021-06-18 11:49:12 +0200

    strItem linked cap

--- frontend/src/app/components/strategy-items/strategy-items.component.ts
+++ frontend/src/app/components/strategy-items/strategy-items.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CapabilityStrategyItems } from 'src/app/classes/capability-strategyitems/capability-strategyitems';
 import { StrategyItem } from 'src/app/classes/strategy-item/strategyItem';
+import { CapabilityStrategyitemService } from 'src/app/services/capability-strategyitem/capability-strategyitem.service';
 import { JwtService } from 'src/app/services/jwt/jwt.service';
 import { NavbarService } from 'src/app/services/navbar/navbar.service';
 import { StrategyItemService } from 'src/app/services/strategy-item/strategy-item.service';
@@ -14,7 +16,10 @@ export class StrategyItemsComponent implements OnInit {
 
   strategyItems: StrategyItem[]
 
-  constructor(private cs: StrategyItemService, private router: Router, private ns: NavbarService, public jwt: JwtService) {
+   capStrItems:  CapabilityStrategyItems[]
+
+  constructor(private cs: StrategyItemService, private router: Router, private ns: NavbarService, public jwt: JwtService,
+    private csi: CapabilityStrategyitemService ) {
     this.strategyItems = [];
   }
 
@@ -34,8 +39,20 @@ export class StrategyItemsComponent implements OnInit {
   showStrItemDelete: boolean = false;
   strItemCurrentValues: StrategyItem;
 
- 
+    // Linked caps right panel
+    showLinkedCaps: boolean = false;
 
+  showLinkedCapabilities(strItemName: string): void {
+    this.csi.getCapabilityStrategyItemsLinkedToStrategyItem(strItemName)
+      .subscribe(response => {
+        this.capStrItems = response;
+        this.show('linked-caps');
+      });
+  }
+
+  setCapabilityCookie(capabilityId: string): void {
+    this.ns.setCapabilityCookie(capabilityId);
+  }
   show(component: string, strategyItem?: StrategyItem): void {
     switch (component) {
       case 'strategyItem-add':
@@ -47,6 +64,7 @@ export class StrategyItemsComponent implements OnInit {
         // Hide
         this.showStrItemAdd = false;
         this.showStrItemDelete = false;
+        this.showLinkedCaps = false;
         // Show
         this.strItemCurrentValues = strategyItem;
         this.showStrItemEdit = !this.showStrItemEdit;
@@ -56,10 +74,16 @@ export class StrategyItemsComponent implements OnInit {
           // Hide
           this.showStrItemAdd = false;
           this.showStrItemEdit = false;
+          this.showLinkedCaps = false;
           // Show
           this.strItemCurrentValues = strategyItem;
           this.showStrItemDelete = !this.showStrItemDelete;
           break;
+          case 'linked-caps':
+            this.hideAll();
+            // Show
+            this.showLinkedCaps = true;
+            break;
       default:
         break;
     }
@@ -69,6 +93,8 @@ export class StrategyItemsComponent implements OnInit {
     this.showStrItemAdd = false;
     this.showStrItemEdit = false;
     this.showStrItemDelete = false;
+
+    this.showLinkedCaps = false;
   }
 
 }
